ff00767349485f1523247371e370df2e262aedc3
Yoda <basinyordan@gmail.com>
2024-09-29 17:30:05 +0300

    fix: locals

--- locals.tf
+++ locals.tf
@@ -1,9 +1,7 @@
 locals {
   gateways = {
-    for service in data.tfe_outputs.tfe.nonsensitive_values.services : 
-    service.name => {
+    for service in data.tfe_outputs.tfe.nonsensitive_values.services : service.name => {
       port = try(service.port, 80)
-    }
-    if service.type == "gateway"
+    } if service.type == "gateway"
   }
 }
